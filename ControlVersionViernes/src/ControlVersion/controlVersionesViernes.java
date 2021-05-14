790e8fd9bce22b349a515d462faba2afc517090c
Profe Invitado_a <Profe Invitado_a@DESKTOP-0FE7I1O>
2021-05-14 09:16:57 +0100

    Commit. Inico de mi programa

---
+++ ControlVersionViernes/src/ControlVersion/controlVersionesViernes.java
@@ -0,0 +1,18 @@
+package ControlVersion;
+
+import java.util.Scanner;
+
+public class controlVersionesViernes {
+
+	public static void main(String[] args) {
+		   Scanner en = new Scanner(System.in);
+	        int numero;
+	            
+	        System.out.println("Introducir un número entero");
+	        numero = en.nextInt();
+	        
+	        System.out.println("El cuadrado de " + numero + " es " + numero*numero);
+
+	}
+
+}
